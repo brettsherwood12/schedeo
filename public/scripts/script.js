f4251ffdbe4a9be201b9124f7a59b123ce4bbd50
Brett Sherwood <clockmakerbrett@gmail.com>
2020-08-04 05:57:11 -0400

    added dynamic form in create event

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,17 @@
-window.addEventListener('load', () => {
-  console.log('Ironmaker app started successfully!');
-}, false);
+$("#addRow").click(function() {
+  var html = "";
+  html += '<div id="inputFormRow">';
+  html += '<div class="input-group mb-3">';
+  html += '<input type="date" name="date[]" class="form-control m-input">';
+  html += '<div class="input-group-append">';
+  html += '<button id="removeRow" type="button" class="btn btn-danger">Remove</button>';
+  html += "</div>";
+  html += "</div>";
+  $("#newRow").append(html);
+});
+
+$(document).on("click", "#removeRow", function() {
+  $(this)
+    .closest("#inputFormRow")
+    .remove();
+});
