84f70dfb1d8b0af835bd1cb58317f37857a5ab0a
Lucia Duarte <analuciacfduarte@gmail.com>
2020-08-04 15:23:06 +0100

    allowed multiple dates to be selected

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@
 const { Router } = require('express');
 const router = new Router();
 const routeGuard = require('./../middleware/route-guard');
+const Event = require('../models/event');
 
 router.get('/', (req, res, next) => {
   res.render('index');
@@ -14,6 +15,46 @@ router.get('/private', routeGuard, (req, res, next) => {
 
 router.get('/profile', (req, res, next) => {
   res.render('user');
+});
+
+//change next route to event.js
+
+// router.post('/event/:id', (req, res) => {
+//   const id = req.params.id;
+//   const idDate = req.body.id;
+
+//   let parentEvent;
+//   Event.findById(id)
+//     .then(parent => {
+//       parentEvent = parent;
+//       let doc;
+//       return (doc = parent.dates.id(idDate));
+//     })
+//     .then(doc => {
+//       doc.votes++;
+//       parentEvent.markModified(doc);
+//       parentEvent.save();
+//     });
+// });
+
+router.post('/event/:id', (req, res) => {
+  const id = req.params.id;
+  const idDate = req.body.id;
+
+  for (let i = 0; i < idDate.length; i++) {
+    let parentEvent;
+    Event.findById(id)
+      .then(parent => {
+        parentEvent = parent;
+        let doc;
+        return (doc = parent.dates.id(idDate[i]));
+      })
+      .then(doc => {
+        doc.votes++;
+        parentEvent.markModified(doc);
+        parentEvent.save();
+      });
+  }
 });
 
 module.exports = router;
