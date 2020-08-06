48b774dd4c0e0b013bf1703f0e8f2fd3db84847f
Lucia Duarte <analuciacfduarte@gmail.com>
2020-08-06 12:08:49 +0100

    added creators events view, created partials

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,68 +1,68 @@
-"use strict";
+'use strict';
 
-const { Router } = require("express");
+const { Router } = require('express');
 
-const passport = require("passport");
+const passport = require('passport');
 
 const router = new Router();
 
-router.get("/sign-up", (req, res) => {
-  res.render("sign-up");
+router.get('/sign-up', (req, res) => {
+  res.render('sign-up');
 });
 
 router.post(
-  "/sign-up",
-  passport.authenticate("local-sign-up", {
-    successRedirect: "/",
-    failureRedirect: "/sign-up",
+  '/sign-up',
+  passport.authenticate('local-sign-up', {
+    successRedirect: '/',
+    failureRedirect: '/sign-up'
   })
 );
 
-router.get("/sign-in", (req, res) => {
-  res.render("sign-in");
+router.get('/sign-in', (req, res) => {
+  res.render('sign-in');
 });
 
 router.post(
-  "/sign-in",
-  passport.authenticate("local-sign-in", {
-    successRedirect: "/",
-    failureRedirect: "/sign-in",
+  '/sign-in',
+  passport.authenticate('local-sign-in', {
+    successRedirect: '/',
+    failureRedirect: '/sign-in'
   })
 );
 
-router.get("/:id/sign-up", (req, res) => {
-  res.render("sign-up-display");
+router.get('/:id/sign-up', (req, res) => {
+  res.render('sign-up-display');
 });
 
 router.post(
-  "/:id/sign-up",
-  passport.authenticate("local-sign-up", {
-    failureRedirect: "/sign-in",
+  '/:id/sign-up',
+  passport.authenticate('local-sign-up', {
+    failureRedirect: '/sign-in'
   }),
   (req, res) => {
     const id = req.params.id;
-    res.redirect(`/event/${id}`);
+    res.redirect(`/event/${id}/join`);
   }
 );
 
-router.get("/:id/sign-in", (req, res) => {
-  res.render("sign-in-display");
+router.get('/:id/sign-in', (req, res) => {
+  res.render('sign-in-display');
 });
 
 router.post(
-  "/:id/sign-in",
-  passport.authenticate("local-sign-in", {
-    failureRedirect: "/sign-in",
+  '/:id/sign-in',
+  passport.authenticate('local-sign-in', {
+    failureRedirect: '/sign-in'
   }),
   (req, res) => {
     const id = req.params.id;
-    res.redirect(`/event/${id}`);
+    res.redirect(`/event/${id}/join`);
   }
 );
 
-router.post("/sign-out", (req, res) => {
+router.post('/sign-out', (req, res) => {
   req.logout();
-  res.redirect("/");
+  res.redirect('/');
 });
 
 module.exports = router;
