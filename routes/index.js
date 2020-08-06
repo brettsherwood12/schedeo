cbd819b0ee22a811062dfbbbd12734696a0957af
Lucia Duarte <analuciacfduarte@gmail.com>
2020-08-06 22:17:56 +0100

    added more styling and updated the edit profile route

--- routes/index.js
+++ routes/index.js
@@ -1,38 +1,41 @@
-"use strict";
+'use strict';
 
-const { Router } = require("express");
+const { Router } = require('express');
 const router = new Router();
-const routeGuard = require("./../middleware/route-guard");
-const Event = require("../models/event");
+const routeGuard = require('./../middleware/route-guard');
+const Event = require('../models/event');
+const User = require('../models/user');
 
-router.get("/", (req, res, next) => {
-  let user;
+router.get('/', (req, res, next) => {
   if (req.user) {
     Event.find({ invitees: { $in: req.user._id } })
-      .then((events) => {
-        res.render("index", { events });
+      .then(events => {
+        res.render('index', { events });
       })
-      .catch((error) => {
+      .catch(error => {
         next(error);
       });
   } else {
-    res.render("index");
+    res.render('index');
   }
 });
 
-router.get("/profile", routeGuard, (req, res, next) => {
-  let user;
-  if (req.user) {
-    Event.find({ invitees: { $in: req.user._id } })
-      .then((events) => {
-        res.render("user", { events });
-      })
-      .catch((error) => {
-        next(error);
-      });
-  } else {
-    res.render("user");
-  }
+router.get('/profile', routeGuard, (req, res, next) => {
+  res.render('user');
+});
+
+router.post('/profile', routeGuard, (req, res, next) => {
+  const id = req.user._id;
+  const { name, email } = req.body;
+
+  User.findByIdAndUpdate(id, { name, email })
+    .then(() => {
+      res.redirect('/profile');
+    })
+    .catch(error => {
+      console.log(error);
+      next(error);
+    });
 });
 
 module.exports = router;
